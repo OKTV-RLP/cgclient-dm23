bb630a105b55e1f07c6040730b7c9fd0ce0f8642
Lukas Herzog <git@lukasherzog.de>
2023-03-11 19:01:31 +0100

    fix: margin between cards

--- src/frontend/components/Card.svelte
+++ src/frontend/components/Card.svelte
@@ -1,10 +1,10 @@
 <script>
-    export let title;
-    export let description;
+	export let title;
+	export let description;
 </script>
 
-<div class="flex flex-col bg-gray-700 p-3 rounded-md mt-2 max-w-md">
-    <h3 class="text-xl">{title}</h3>
-    <p class="text-sm italic">{description}</p>
-    <slot />
+<div class="flex flex-col bg-gray-700 p-3 rounded-md mt-2 mb-4 max-w-md">
+	<h3 class="text-xl">{title}</h3>
+	<p class="text-sm italic">{description}</p>
+	<slot />
 </div>
