17a8463cd13d913a93aeba0bc9b29dfdafdcfb4b
Lukas Herzog <git@lukasherzog.de>
2023-03-11 18:59:49 +0100

    add Select component

---
+++ src/frontend/components/Select.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let label = '';
+	export let id;
+	export let name;
+	export let value;
+	export let disabled = false;
+	export let options = [];
+</script>
+
+<div class="pb-3 w-full">
+	<label for={id} class="text-sm pb-1 ml-1">{label}</label>
+	<select
+		bind:value
+		{name}
+		{id}
+		{disabled}
+		class="w-full rounded-md bg-gray-600 text-gray-100 border-none focus:shadow-none focus:border-none focus:ring-orange-600 focus:ring-2"
+	>
+		{#each options as option, id}
+			<option value={id}>{option}</option>
+		{/each}
+	</select>
+</div>
