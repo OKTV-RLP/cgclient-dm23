b91738a3797b609e5b5a28cfcd3cc1cd2f40d341
Lukas Herzog <git@lukasherzog.de>
2023-03-11 19:00:32 +0100

    implement functionality for InfoBar

--- src/frontend/cards/InfoBar.svelte
+++ src/frontend/cards/InfoBar.svelte
@@ -1,12 +1,13 @@
 <script>
 	import Card from '../components/Card.svelte';
-	// import Button from '../components/Button.svelte';
-	// import Input from '../components/Input.svelte';
+	import Button from '../components/Button.svelte';
+	import Select from '../components/Select.svelte';
 	import { onMount } from 'svelte';
 
-	let ib_col1 = 0;
-	let ib_col2 = '';
+	let id_col1 = 0;
+	let id_col2 = 0;
 	let infoOptions = [];
+	let tournaments = [];
 	let disciplines = [];
 	let disabled = false;
 
@@ -15,74 +16,67 @@
 	onMount(async () => {
 		// Get Default Values from Config
 		infoOptions = await window.api.getSelectOptions(slot);
-		console.log(infoOptions);
+		// console.log(infoOptions);
 	});
 
-	$: disciplines = infoOptions.length > 0 ? infoOptions[ib_col1].disciplines : [];
+	$: disciplines = infoOptions.length > 0 ? infoOptions[id_col1].disciplines : [];
 	$: disabled = disciplines.length === 0 ? true : false;
+	$: id_col2 = disabled ? '' : 0;
+	$: tournaments = infoOptions.map((options) => options.name);
 
-	// const handlePlay = () => {
-	// 	const data = {
-	// 		slot,
-	// 		auto: false,
-	// 		templateData: [bb_col1, bb_col2],
-	// 		duration: autoPlayTime
-	// 	};
-	// 	// console.log(data);
-	// 	window.api.playCG(data);
-	// };
+	const handlePlay = () => {
+		const ln1 = infoOptions[id_col1].name;
+		const ln2 = disabled ? ' ' : disciplines[id_col2];
+		const data = {
+			slot,
+			auto: false,
+			templateData: [ln1, ln2],
+			duration: 0
+		};
+		// console.log(data);
+		window.api.playCG(data);
+	};
 
-	// const handleStop = () => {
-	// 	window.api.stopCG(slot);
-	// };
+	const handleStop = () => {
+		window.api.stopCG(slot);
+	};
 
-	// const handleClear = () => {
-	// 	window.api.clearCG(slot);
-	// };
+	const handleClear = () => {
+		window.api.clearCG(slot);
+	};
 
-	// const handleAutoPlay = () => {
-	// 	const data = {
-	// 		slot,
-	// 		auto: true,
-	// 		templateData: [bb_col1, bb_col2],
-	// 		duration: autoPlayTime
-	// 	};
-	// 	window.api.playCG(data);
-	// };
+	const handleUpdate = () => {
+		console.log('update');
+	};
 </script>
 
 <Card title="Info-Bar" description="Infobereich mit Wettkampf und Disziplin">
 	<form class="my-2">
-		<label for="ib_col1">Turnier</label>
-		<select
-			bind:value={ib_col1}
+		<Select
+			bind:value={id_col1}
+			label="Turnier"
 			name="tournament"
 			id="ib_col1"
-			class="w-full rounded-md bg-gray-600 text-gray-100 border-none focus:shadow-none focus:border-none focus:ring-orange-600 focus:ring-2"
-		>
-			{#each infoOptions as option, id}
-				<option value={id}>{option.name}</option>
-			{/each}
-		</select>
+			options={tournaments}
+		/>
 
-		<label for="ib_col2">Disziplin</label>
-		<select
-			bind:value={ib_col2}
+		<Select
+			bind:value={id_col2}
+			label="Disziplin"
 			name="discipline"
 			id="ib_col2"
 			{disabled}
-			class="w-full rounded-md bg-gray-600 text-gray-100 border-none focus:shadow-none focus:border-none focus:ring-orange-600 focus:ring-2"
-		>
-			{#each disciplines as discipline}
-				<option>{discipline}</option>
-			{/each}
-		</select>
+			options={disciplines}
+		/>
 
 		<div class="flex flex-row gap-2 mt-2 justify-between h-10 w-full">
 			<div class="flex flex-row gap-2">
-				<!-- <Button style="green" on:click={handlePlay}>Start</Button>
+				<Button style="green" on:click={handlePlay}>Start</Button>
 				<Button style="red" on:click={handleStop}>Stop</Button>
-				<Button style="blue" on:click={handleClear}>!Clear!</Button> -->
+				<Button style="blue" on:click={handleClear}>!Clear!</Button>
+			</div>
+			<div class="flex flex-row justify-end">
+				<Button style="light-green" on:click={handleUpdate}>Update</Button>
 			</div>
 		</div>
 	</form></Card
